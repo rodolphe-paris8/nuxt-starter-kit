11bf983a34a7f62da3ce204913dc387e94227cd8
rodolphe-paris8 <rodolphe-paris8@proton.me>
2025-12-15 14:53:36 -0400

    Updated plugins.d.ts

--- .nuxt/types/plugins.d.ts
+++ .nuxt/types/plugins.d.ts
@@ -20,7 +20,8 @@ type NuxtAppInjections =
   InjectionType<typeof import("../../node_modules/@nuxt/devtools/dist/runtime/plugins/devtools.server.js")> &
   InjectionType<typeof import("../../node_modules/@nuxt/devtools/dist/runtime/plugins/devtools.client.js")> &
   InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/warn.dev.server.js")> &
-  InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/check-if-layout-used.js")>
+  InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/check-if-layout-used.js")> &
+  InjectionType<typeof import("../../plugins/supabase.client")>
 
 declare module '#app' {
   interface NuxtApp extends NuxtAppInjections { }
